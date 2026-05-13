a602df2c2a83439c36e76881220c688ac13efcc9
Rushian <gmquan2006@gmail.com>
2026-05-13 19:50:31 +0700

    Time: 0 ms (100%), Space: 8.6 MB (12.76%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        if (x < 2)
+            return x;
+
+        long long l = 1;
+        long long r = x;
+
+        int ans = 0;
+
+        while (l <= r) {
+
+            long long mid = l + (r - l) / 2;
+
+            if (mid * mid <= x) {
+
+                ans = mid;
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
